ee1cb6460f260335eba7471ea57dd866fef54b39
ombeltran <ombeltranduk@gmail.com>
2023-12-30 20:36:11 -0600

    adjusting resume pdf

--- src/pages/pdf.jsx
+++ src/pages/pdf.jsx
@@ -4,10 +4,10 @@ export default function Pdf() {
     const pdfUrl = `${pdfImage}#toolbar=0`;
     
     return (
-        <div className="pdf-container absolute mt-14 w-[90%] md:w-screen h-screen mx-auto">
+        <div className="pdf-container absolute mt-14 w-screen md:w-screen h-screen mx-auto">
             <button className="absolute md:bg-black bg-red-800 font-bold w-36 h-12 rounded-full
             border-solid border-4 border-white border-opacity-53 hover:scale-125 z-20 md:mt-[36%] md:ml-[86%]
-            ml-[62%] mt-[200%] ">
+            ml-[62%] mt-[185%] ">
                 <a href={pdfImage} download="Resume_Oscar_Beltran.pdf">
                     PDF download
                 </a>
