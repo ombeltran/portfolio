4066a282b31338cfebe54193c446b81142bda0cc
ombeltran <ombeltranduk@gmail.com>
2024-01-15 22:49:44 -0600

    update resume PDF

--- src/pages/listProjects.js
+++ src/pages/listProjects.js
@@ -1,8 +1,8 @@
 export const myListProjects = [
     {
         name: "Jamming_project_omb",
-        description: "Project Jamming with React, this get a music API that give you a music list of your favorite singer",
-        tecnology: "React js and Node js",
+        description: "Project Jamming with React, this get a music API that give you a music list of your favorite singer.",
+        tecnology: "React js.",
         git: "https://github.com/ombeltran/jamming_project_omb",
         url: "https://jamming-sq78.onrender.com/"
     },
@@ -16,7 +16,7 @@ export const myListProjects = [
     {
         name: "Restaurant Fast Food",
         description: "Website of a fast food store, which shows its products, has a shopping cart and allows you to place an order via WhatsApp API.",
-        tecnology: "React js + Vite and Tailwind",
+        tecnology: "React js + Vite and Tailwind.",
         git: "https://github.com/ombeltran/restaurant.git",
         url: "https://restaurant-ncx1.onrender.com/"
     }
