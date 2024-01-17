24bbc6acac3fed1f5eb2923df64bff0ed7335ebd
ombeltran <ombeltranduk@gmail.com>
2024-01-16 22:11:40 -0600

    fixed layout pdf

--- src/pages/pdf.jsx
+++ src/pages/pdf.jsx
@@ -2,9 +2,9 @@ import pdfImage from "../sources/pdf/Oscar Marino Beltran Dukmak.pdf";
 
 export default function Pdf() {
     const pdfUrl = `${pdfImage}#toolbar=0`;
-    
+
     return (
-        <div className="pdf-container absolute mt-14 w-screen md:w-screen h-screen mx-auto">
+        <div className="absolute mt-14 w-screen h-screen mx-auto">
             <button className="absolute md:bg-black bg-red-800 font-bold w-36 h-12 rounded-full
             border-solid border-4 border-white border-opacity-53 hover:scale-125 z-20 md:mt-[36%] md:ml-[86%]
             ml-[62%] mt-[185%] ">
@@ -12,11 +12,11 @@ export default function Pdf() {
                     PDF download
                 </a>
             </button>
-            <object 
-            data={pdfUrl} 
-            type="application/pdf"
-            width="100%"
-            height="100%"
+            <object
+                data={pdfUrl}
+                type="application/pdf"
+                width="100%"
+                height="100%"
             >
             </object>
         </div>
