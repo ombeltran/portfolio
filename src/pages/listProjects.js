4cc739e2876e8a30619311ae8c55662fe77f0f1d
ombeltran <ombeltranduk@gmail.com>
2024-01-19 18:48:09 -0600

    updating project descriptions

--- src/pages/listProjects.js
+++ src/pages/listProjects.js
@@ -1,21 +1,21 @@
 export const myListProjects = [
     {
         name: "Jamming_project_omb",
-        description: "Project Jamming with React, this get a music API that give you a music list of your favorite singer.",
+        description: "Project that allows you to obtain a music API that gives you a list of songs by your favorite singer.",
         tecnology: "React js.",
         git: "https://github.com/ombeltran/jamming_project_omb",
         url: "https://jamming-sq78.onrender.com/"
     },
     {
         name: "ProdMonitor",
-        description: "Application with a CRUD system that permit to get a report of a inspection process. It was deploy on hosting web.",
+        description: "Application with CRUD system with authentication that allows obtaining a productivity report of an inspection process.",
         tecnology: "React js, Node js and postgreSQL.",
         git: "https://github.com/ombeltran/ProdMonitor.git",
         url: "https://frontend-prodmonitor.onrender.com/"
     },
     {
         name: "Restaurant Fast Food",
-        description: "Website of a fast food store, which shows its products, has a shopping cart and allows you to place an order via WhatsApp API.",
+        description: "Website of a fast food store, which shows its products, has a shopping cart and allows you to place an order, the information of which is sent to WhatsApp",
         tecnology: "React js + Vite and Tailwind.",
         git: "https://github.com/ombeltran/restaurant.git",
         url: "https://restaurant-ncx1.onrender.com/"
